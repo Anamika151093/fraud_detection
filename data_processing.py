c83529ad12694f2625c2ddddefa41c1245fc8453
anamika151093 <anamika151093@gmail.com>
2023-01-12 11:20:48 +0530

    new sum function

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,8 @@
 print("adding all the preprocessing code related to fraud detection ")
+
+a = 5
+b = 4
+def sum(a,b):
+    c = a+b
+    return c
+    print("added a new function sum which is not correct")
